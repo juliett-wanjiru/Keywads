c70fb030597c6c0e93665041cc3c321a698497f1
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-02-17 11:43:38 +0300

    my work

---
+++ src/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var studentName:String="juliet wanjiru otieno"
+    println(studentName)
+    var age=19
+    println(age)
+    var phoneNumber:String="0798007612"
+    println(phoneNumber)
+    var weight=60
+    println(weight)
+    val kenyan:Boolean=true
+    println(kenyan)}
+
